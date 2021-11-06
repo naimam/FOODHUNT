7e4c7983376682bdc25b2577e2b94b1fbc1bc2c8
Thu Vo <tvo55@student.gsu.edu>
2021-11-06 01:13:26 -0400

    add search button and search modal

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,22 @@
-import React from 'react';
-import { Nav, Navbar } from 'react-bootstrap';
+import { useState } from 'react';
+import './NavBar.css';
+import { Nav, Navbar, NavDropdown, Button, Form, Modal } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
 import { Link } from 'react-router-dom';
 import logo from '../logo.svg';
 
-export default class NavBar extends React.Component {
-    render() {
-        return (<>
+
+function NavBar() {
+    const [showModal, setShowModal] = useState(false);
+    const openSearch = () => {
+        setShowModal(true)
+    }
+    const hideSearch = () => {
+        setShowModal(false)
+    }
+    return (
+        <>
             <Navbar collapseOnSelect bg="dark" expand="lg" variant="dark">
                 <Navbar.Brand as={Link} to="/home">
                     <img
@@ -21,9 +32,51 @@ export default class NavBar extends React.Component {
                     <Nav className="me-auto">
                         <Nav.Link as={Link} to="/home">Home</Nav.Link>
                         <Nav.Link as={Link} to="/favorite">Favorite</Nav.Link>
+                        <NavDropdown title="Profile" id="basic-nav-dropdown">
+                            <NavDropdown.Item>Signed in as: john_martin</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.4">Log out</NavDropdown.Item>
+                        </NavDropdown>
                     </Nav>
+
+                    <Form inline className="pe-md-3" >
+                        <Button onClick={openSearch} variant="primary"> <FontAwesomeIcon icon={faSearch} /> Search</Button>
+                    </Form>
+
                 </Navbar.Collapse>
             </Navbar>
-        </>)
-    }
+
+            <Modal
+                show={showModal}
+                onHide={hideSearch}
+                size="xl"
+                centered
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title>
+                        Custom Modal Styling
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <p>
+                        Ipsum molestiae natus adipisci modi eligendi? Debitis amet quae unde
+                        commodi aspernatur enim, consectetur. Cumque deleniti temporibus
+                        ipsam atque a dolores quisquam quisquam adipisci possimus
+                        laboriosam. Quibusdam facilis doloribus debitis! Sit quasi quod
+                        accusamus eos quod. Ab quos consequuntur eaque quo rem! Mollitia
+                        reiciendis porro quo magni incidunt dolore amet atque facilis ipsum
+                        deleniti rem! Ipsum molestiae natus adipisci modi eligendi? Debitis amet quae unde
+                        commodi aspernatur enim, consectetur. Cumque deleniti temporibus
+                        ipsam atque a dolores quisquam quisquam adipisci possimus
+                        laboriosam. Quibusdam facilis doloribus debitis! Sit quasi quod
+                        accusamus eos quod. Ab quos consequuntur eaque quo rem! Mollitia
+                        reiciendis porro quo magni incidunt dolore amet atque facilis ipsum
+                        deleniti rem!
+                    </p>
+                </Modal.Body>
+            </Modal>
+        </>
+    );
 }
+
+export default NavBar
