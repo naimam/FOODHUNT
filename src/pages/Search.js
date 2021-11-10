686e000ebceebe68de69711befd47943a0e52853
naimam <naimamohamed19@hotmail.com>
2021-11-10 16:02:12 +0000

    updating search page to display recipe results

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import Recipe from '../components/Recipe';
 
 function Search() {
     const { option } = useParams();
     const { keyword } = useParams();
     const { zip } = useParams();
 
+    if (option === 'recipe') {
+        return (
+            <Recipe keyword={keyword} />
+        );
+    }
     return (<>
         <h1>Search Result Page !!</h1>
         <h2>Option: {option}</h2>
