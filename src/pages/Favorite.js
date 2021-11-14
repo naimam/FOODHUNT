27bdd19134521a85dc4d07227ce838b269fe6a34
Andrew Yen <24379988+yenandrew@users.noreply.github.com>
2021-11-14 09:38:33 -0500

    Update Favorite.js

--- src/pages/Favorite.js
+++ src/pages/Favorite.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+import { Card, Row, Col, Container } from 'react-bootstrap';
 
-export default class Favorite extends React.Component {
-    render() {
-        return (<h1>FAVORITE PAGE !!</h1>)
-    }
-}
+function Favorites(props) {
+    const [recipeData, setRecipeData] = useState([])
+    const [restaurantData, setRestaurantData] = useState([])
+    
+User.query.filter_by(username=hello.username).first()
+db.session.add(Recipe(recipe_id="testid" user_id=user.user_id))
+db.session.add(Restaurant(restaurant_id="testid" user_id=user.user_id))
+db.session.commit()
+
+export default Favorites
